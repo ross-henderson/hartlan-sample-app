786fa7fa98048ea61338fc5807f30a7ecaf9fe8c
Ross Henderson <ross.henderson@me.com>
2016-05-15 15:23:18 -0500

    Add new users page, route, and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
